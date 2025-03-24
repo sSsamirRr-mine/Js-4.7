f2e0fd8923ca323bac3d98e2a5fbef3403f5a3dd
sSsamirRr-mine <sssamirrr4@gmail.com>
2025-03-24 15:43:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var typed = new Typed('#one', {
+    strings: ["Netflix"],
+    typeSpeed: 20
+});
+
+var typed = new Typed('#two', {
+    strings: ["Splash Screen"],
+    typeSpeed: 30
+});
